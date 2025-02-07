a1672c3afe247e6ee7c7196c38f88bfd7ab87ca8
LT-LuciaT <lucia.trombin98@gmail.com>
2025-02-07 15:38:46 +0100

    6

--- src/components/AreaFilm.jsx
+++ src/components/AreaFilm.jsx
@@ -1,19 +1,48 @@
 import { Component } from "react";
+import { Button, Card } from "react-bootstrap";
 
 class AreaFilm extends Component {
-  fetchSeries = async () => {
-    const resp = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=b55e94bb", {
+  state = {
+    films: [],
+  };
+
+  fetchMovie = async () => {
+    const resp = await fetch("http://www.omdbapi.com/?apikey=b55e94bb&s=hunger", {
       method: "GET",
     });
 
     if (resp.ok) {
-      const film = await resp.json();
-      console.log(film);
+      const data = await resp.json();
+      if (data.Search) {
+        this.setState({ films: data.Search.slice(0, 5) });
+      }
     }
   };
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   render() {
-    this.fetchSeries();
-    return <div className="text-light">ecco i film</div>;
+    const { films } = this.state;
+    return (
+      <div className="text-light">
+        <div className="film-cards d-flex flex-wrap">
+          {films.map((film, index) => (
+            <Card key={index} style={{ width: "18rem", margin: "10px" }}>
+              <Card.Img variant="top" src={film.Poster} alt={film.Title} />
+              <Card.Body>
+                <Card.Title>{film.Title}</Card.Title>
+                <Card.Text>{film.Year}</Card.Text>
+                <Button variant="primary" href={`https://www.imdb.com/title/${film.imdbID}`} target="_blank">
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
