8834ac587831b8c68590d1fb951bc3897f678787
LT-LuciaT <lucia.trombin98@gmail.com>
2025-02-07 12:06:57 +0100

    2

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,20 +1,38 @@
+import "../App.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Search } from "react-bootstrap-icons";
 
 const MyNav = () => {
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand href="NETFLIX"></Navbar.Brand>
+      <Container fluid>
+        <Navbar.Brand href=""> NETFLIX</Navbar.Brand>
+        <Navbar className="gap-3">
+          <Nav.Link href="#home" className="text-light">
+            Home
+          </Nav.Link>
+          <Nav.Link href="#link" className="text-light">
+            TV Shows
+          </Nav.Link>
+          <Nav.Link href="#link" className="text-light">
+            Movies
+          </Nav.Link>
+          <Nav.Link href="#link" className="text-light">
+            Recently Added
+          </Nav.Link>
+          <Nav.Link href="#link" className="text-light">
+            My List
+          </Nav.Link>
+        </Navbar>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">TV Shows</Nav.Link>
-            <Nav.Link href="#link">Movies</Nav.Link>
-            <Nav.Link href="#link">Recently Added</Nav.Link>
-            <Nav.Link href="#link">My List</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="#link" className="text-white">
+              <Search />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
